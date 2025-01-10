cd44908bd7a59969ee1611adbfe66bbb06d7888d
Leonor Gothen <lcgo@mir-robots.com>
2025-01-10 15:04:34 +0100

    Final skeleton

---
+++ dotnet_vet_clinic/Filing/Models/Employee.cs
@@ -0,0 +1,5 @@
+namespace Filing.Models;
+
+public class Employee(string name, string address, string phoneNumber) : Person(name, address, phoneNumber)
+{
+}
--- dotnet_vet_clinic/Filing/Models/Person.cs
+++ dotnet_vet_clinic/Filing/Models/Person.cs
@@ -1,11 +1,15 @@
-namespace Filing.Models
+namespace Filing.Models;
+
+public class Person(string name, string address, string phoneNumber)
 {
-    public class Person(string name, string address, string phoneNumber)
+    private readonly Guid _id = Guid.NewGuid();
+
+    public string Name { get; set; } = name;
+    public string Address { get; set; } = address;
+    public string PhoneNumber { get; set; } = phoneNumber;
+
+    public string GetId()
     {
-        public string Name { get; set; } = name;
-        public string Address { get; set; } = address;
-        public string PhoneNumber { get; set; } = phoneNumber;
-        
-        private Guid _id = Guid.NewGuid();
+        return _id.ToString();
     }
 }
